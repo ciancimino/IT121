de6e112456c8d666ff0f029a29b5fb20cf36ea7f
Alexandra Ciancimino <noreply>
2024-01-07 19:16:24 -0800

    added week 1 folder and files

---
+++ week1/script.js
@@ -0,0 +1,4 @@
+var firstname = "Alexandra";
+var lastname = "Ciancimino";
+var yearsofstudy = "0.5";
+var goal = "to gain a comprehensive understanding of web design and further refine my skills in this area. As someone with a background in tech startups but only in non-technical roles, I believe enhancing my web design skills will contribute significantly to my career growth."
